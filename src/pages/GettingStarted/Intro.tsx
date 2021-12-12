18ce61951fc6d80644f47709add8345a15440a12
Max <Serenity09@users.noreply.github.com>
2021-12-11 23:02:40 -0500

    Removed display headers
    Intro now uses header styling but span html

--- src/pages/GettingStarted/Intro.tsx
+++ src/pages/GettingStarted/Intro.tsx
@@ -1,10 +1,12 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Intro() {
     return (
         <>
-            <Typography variant="h1" sx={{ display: "inline-block" }}>Navigating</Typography>
-            <Typography variant="h2" sx={{ display: "inline"}}> the United States healthcare system sucks, so that's basically why this guide exists. </Typography>
+            <Box sx={{mb: "2rem"}}>
+                <Typography variant="h1" component="span">Navigating</Typography>
+                <Typography variant="h2" component="span"> the United States healthcare system sucks, so that's basically why this guide exists.</Typography>
+            </Box>
         </>
     );
 }
