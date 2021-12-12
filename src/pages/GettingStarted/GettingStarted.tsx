860347baebfb8d23d38f2fe84ded79d2a4ef19b4
Max <Serenity09@users.noreply.github.com>
2021-12-11 23:46:07 -0500

    Moved stepper to own file

--- src/pages/GettingStarted/GettingStarted.tsx
+++ src/pages/GettingStarted/GettingStarted.tsx
@@ -1,71 +1,23 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Navigate, useLocation } from 'react-router-dom';
 
 import IUseUserInfo from '../../interfaces/IUseUserInfo';
 
+import GettingStartedStepper from './GettingStartedStepper';
 import Intro from './Intro';
 import WhatAboutYou from './WhatAboutYou';
 import TheGoodPart from './TheGoodPart';
 
 export default function GettingStarted(props: IUseUserInfo) {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const location = useLocation();
-
-  const steps = [{title: 'Intro', element: <Intro />}, {title: 'What about you?', element: <WhatAboutYou {...props} />}, {title: 'The good part', element: <TheGoodPart {...props.userInfo} />}];
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
+  const steps = [
+    {title: 'Intro', element: <Intro />}, 
+    {title: 'What about you?', element: <WhatAboutYou {...props} />}, 
+    {title: 'The good part', element: <TheGoodPart {...props.userInfo} />}
+  ];
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep} sx={{ mb: "1rem" }}>
-        {steps.map((step, index) => {
-          const stepProps: { completed?: boolean } = {};
-          const labelProps: {
-            optional?: React.ReactNode;
-          } = {};
-          return (
-            <Step key={step.title} {...stepProps}>
-              <StepLabel {...labelProps}>{step.title}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <Navigate to='/guide' state={{ from: location }} />
-      ) : (
-        <React.Fragment>
-          {steps[activeStep].element}
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? "Let's go" : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
+      <GettingStartedStepper steps={steps} />
     </Box>
   );
 }
---
+++ src/pages/GettingStarted/GettingStartedStepper.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import { Navigate, useLocation } from 'react-router-dom';
+
+interface GettingStartedStepperSettings {
+  steps: Array<{
+    title: string,
+    element: JSX.Element
+  }>
+}
+
+export default function GettingStartedStepper(props: GettingStartedStepperSettings) {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const location = useLocation();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <>
+      <Stepper activeStep={activeStep} sx={{ mb: "1rem" }}>
+        {props.steps.map((step, index) => {
+          const stepProps: { completed?: boolean } = {};
+          const labelProps: {
+            optional?: React.ReactNode;
+          } = {};
+          return (
+            <Step key={step.title} {...stepProps}>
+              <StepLabel {...labelProps}>{step.title}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === props.steps.length ? (
+        <Navigate to='/guide' state={{ from: location }} />
+      ) : (
+        <React.Fragment>
+          {props.steps[activeStep].element}
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleNext}>
+              {activeStep === props.steps.length - 1 ? "Let's go" : 'Next'}
+            </Button>
+          </Box>
+        </React.Fragment>
+      )}
+    </>
+  );
+}
