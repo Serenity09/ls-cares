df737f0b3d4e27a21437cae083def33ed7b1cd77
Max <Serenity09@users.noreply.github.com>
2021-12-12 19:41:49 -0500

    Cookie support added
    Disclaimer selection now remembered via cookie
    Cookie expires with new disclaimer version or after 7 days of last acceptance

--- src/pages/Disclaimer/Disclaimer.tsx
+++ src/pages/Disclaimer/Disclaimer.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -7,7 +8,8 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { createStyles, makeStyles } from '@mui/styles';
-import { useState } from 'react';
+
+import Cookies from 'js-cookie';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -19,48 +21,74 @@ const useStyles = makeStyles(() =>
     disclaimerBlock: {
       marginTop: '1rem'
     }
-  }));
+  })
+);
+
+//store user's disclaimer selection in a cookie
+const ACCEPTED_DISCLAIMER_COOKIE_NAME = "acceptedDisclaimer";
+const VERSION = 1;
+const DISCLAIMER_EXPIRATION = 7; //in days
+
+let defaultDisclaimerOpen = true;
+try {
+  const cookie = Cookies.get(ACCEPTED_DISCLAIMER_COOKIE_NAME);
+
+  if (cookie !== undefined) {
+    const acceptedDisclaimerCookie = JSON.parse(cookie?? "{}");
+    const acceptedDisclaimerDate = new Date(acceptedDisclaimerCookie.date);
+
+    if (acceptedDisclaimerCookie.version === VERSION 
+        && new Date(acceptedDisclaimerDate.getFullYear(), acceptedDisclaimerDate.getMonth(), acceptedDisclaimerDate.getDate() + DISCLAIMER_EXPIRATION, acceptedDisclaimerDate.getHours(), acceptedDisclaimerDate.getMinutes(), acceptedDisclaimerDate.getSeconds()) >= new Date())
+      defaultDisclaimerOpen = false;
+    else
+      Cookies.remove(ACCEPTED_DISCLAIMER_COOKIE_NAME);
+  }
+}
+catch (err) { }
 
 export default function Disclaimer() {
   const classes = useStyles();
-
-  const [ isDisclaimerOpen, setIsDisclaimerOpen ] = useState(true);
-  const [ acceptedDisclaimer, setAcceptedDisclaimer ] = useState(true);
+  const [ isDisclaimerOpen, setIsDisclaimerOpen ] = useState(defaultDisclaimerOpen);
 
   const closeDisclaimer = function(acceptedDisclaimer: boolean) {
-    if (acceptedDisclaimer)
+    if (acceptedDisclaimer) {
       setIsDisclaimerOpen(false);
-    else
-      setAcceptedDisclaimer(false);
+
+      Cookies.set(ACCEPTED_DISCLAIMER_COOKIE_NAME, JSON.stringify({
+        date: new Date(),
+        version: 1
+      }));
+    }
   }
 
   return (
-    <div>
-      <Dialog
-        open={isDisclaimerOpen}
-        onClose={() => closeDisclaimer(false)}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">Disclaimer</DialogTitle>
-        <DialogContent className={classes.disclaimerContent}>
-          <DialogContentText className={classes.disclaimerBlock}>
-            The information available on Livestream Cares (LSC) may include inaccuracies, omissions, and other errors. LSC only provides information regarding health insurance coverage and is not intended nor implied to substitute professional advice. We are not lawyers and this should not be considered legal advice. We are not doctors and this should not be considered medical advice. Please contact the appropriate professional for any personal, financial, medical, health, or legal decisions.
-          </DialogContentText>
-          <DialogContentText className={classes.disclaimerBlock}>
-            We hope LSC helps make you a more knowledgeable health insurance consumer. We make no representations about the accuracy, content, reliability, completeness, or legality of this information for any purpose. We cannot guarantee insurance coverage or financial exposure for any health services.
-          </DialogContentText>
-          <DialogContentText className={classes.disclaimerBlock}>
-            In no event shall the owners of or contributors to LSC be liable for direct, indirect, punitive, incidental, special, consequential damages or any damages whatsoever, arising out of or connected with the use of this website or the information contained herein. LSC contains links to third party websites. We cannot control the information provided on those websites and we will not take any responsibility for the information or content thereon.
-          </DialogContentText>
-          <DialogContentText className={classes.disclaimerBlock}>
-            Last updated 12/10/2021
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => closeDisclaimer(true)} autoFocus>I Accept</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+    <Dialog
+      open={isDisclaimerOpen}
+      onClose={() => closeDisclaimer(false)}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">Disclaimer</DialogTitle>
+      <DialogContent className={classes.disclaimerContent}>
+        <DialogContentText className={classes.disclaimerBlock}>
+          The information available on Livestream Cares (LSC) may include inaccuracies, omissions, and other errors. LSC only provides information regarding health insurance coverage and is not intended nor implied to substitute professional advice. We are not lawyers and this should not be considered legal advice. We are not doctors and this should not be considered medical advice. Please contact the appropriate professional for any personal, financial, medical, health, or legal decisions.
+        </DialogContentText>
+        <DialogContentText className={classes.disclaimerBlock}>
+          We hope LSC helps make you a more knowledgeable health insurance consumer. We make no representations about the accuracy, content, reliability, completeness, or legality of this information for any purpose. We cannot guarantee insurance coverage or financial exposure for any health services.
+        </DialogContentText>
+        <DialogContentText className={classes.disclaimerBlock}>
+          In no event shall the owners of or contributors to LSC be liable for direct, indirect, punitive, incidental, special, consequential damages or any damages whatsoever, arising out of or connected with the use of this website or the information contained herein. LSC contains links to third party websites. We cannot control the information provided on those websites and we will not take any responsibility for the information or content thereon.
+        </DialogContentText>
+        <DialogContentText className={classes.disclaimerBlock}>
+          In short, we're trying our best, but we can't deal with legal issues.
+        </DialogContentText>
+        <DialogContentText className={classes.disclaimerBlock}>
+          Last updated 12/12/2021
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => closeDisclaimer(true)} autoFocus>I Accept</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
